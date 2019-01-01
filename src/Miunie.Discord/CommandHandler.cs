58a9169322f5000720297529dbf3cdf1c3fd41a9
Charly6596 <cgutisanc@gmail.com>
2019-01-01 06:49:41 +0100

    Implement the command handler and add dependencies to MiunieDiscord

---
+++ src/Miunie.Discord/CommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+
+namespace Miunie.Discord
+{
+    public class CommandHandler
+    {
+        private CommandsNextModule _commandService;
+
+        public CommandHandler(CommandsNextModule commandService)
+        {
+            _commandService = commandService;
+        }
+
+        public async Task InitializeAsync()
+        {
+            /*
+             TODO (Charly): Here we should register the commands,
+             using the _commandService.RegisterCommands method
+             https://dsharpplus.emzi0767.com/api/DSharpPlus.CommandsNext.CommandsNextModule.html#DSharpPlus_CommandsNext_CommandsNextModule_RegisterCommands_Assembly_
+            */
+        }
+    }
+}
--- src/Miunie.Discord/MiunieDiscord.cs
+++ src/Miunie.Discord/MiunieDiscord.cs
@@ -4,6 +4,13 @@ namespace Miunie.Discord
 {
     public class MiunieDiscord : IDiscord
     {
+        private DSharpClient _client;
+        private CommandHandler _commandHandler;
 
+        public MiunieDiscord(DSharpClient client, CommandHandler commandHandler)
+        {
+            _client = client;
+            _commandHandler = commandHandler;
+        }
     }
 }
