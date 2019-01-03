dd098d8296e49038f87bbebfba08fdd2fafd41ed
Petr Sedláček <petr.spelos@gmail.com>
2019-01-03 10:56:24 +0100

    Remove commented out code

--- src/Miunie.ConsoleApp/InversionOfControl.cs
+++ src/Miunie.ConsoleApp/InversionOfControl.cs
@@ -32,21 +32,6 @@ namespace Miunie.ConsoleApp
         {
             container = new Container(c =>
             {
-                // c.ForSingletonOf<Logger>().UseIfNone<Logger>();
-                // c.ForSingletonOf<TriviaGames>().UseIfNone<TriviaGames>();
-                // c.ForSingletonOf<DiscordEventHandler>().UseIfNone<DiscordEventHandler>();
-                // c.ForSingletonOf<CommandHandler>().UseIfNone<CommandHandler>();
-                // c.ForSingletonOf<CommandService>().UseIfNone<CommandService>();
-                // c.ForSingletonOf<DiscordSocketClient>().UseIfNone(DiscordClientFactory.GetBySettings(settings));
-                // c.ForSingletonOf<ApplicationSettings>().UseIfNone(settings);
-                // c.ForSingletonOf<IDataStorage>().UseIfNone<JsonDataStorage>();
-                // c.ForSingletonOf<ListManager>().UseIfNone<ListManager>();
-                // c.ForSingletonOf<IOnboarding>().UseIfNone<Onboarding>();
-                // c.ForSingletonOf<Features.Onboarding.Tasks.HelloWorldTask>().UseIfNone<Features.Onboarding.Tasks.HelloWorldTask>();
-                // c.ForSingletonOf<IGlobalUserAccountProvider>().UseIfNone<GlobalUserAccountProvider>();
-                // c.ForSingletonOf<IDiscordSocketClient>().UseIfNone<DiscordSocketClientAbstraction>();
-                // c.ForSingletonOf<IDailyMiunies>().UseIfNone<Daily>();
-                // c.ForSingletonOf<IMiuniesTransfer>().UseIfNone<Transfer>();
                 c.ForSingletonOf<IDiscord>().UseIfNone<DSharpPlusDiscord>();
                 c.ForSingletonOf<IBotConfiguration>().UseIfNone<BotConfiguration>();
                 c.ForSingletonOf<IConfiguration>().UseIfNone<ConfigManager>();
