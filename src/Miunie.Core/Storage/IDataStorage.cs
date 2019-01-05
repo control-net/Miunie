58703ed24ad6a57bc25350f0012664664b94e9a6
Carlos J. Gutiérrez Sánchez <cgutisanc@gmail.com>
2019-01-05 20:45:25 +0100

    Add Miunie.Storage project

---
+++ src/Miunie.Core/Storage/IDataStorage.cs
@@ -0,0 +1,8 @@
+namespace Miunie.Core.Storage
+{
+    public interface IDataStorage
+    {
+        void StoreObject(object obj, string key);
+        T RestoreObject<T>(string key);
+    }
+}
---
+++ src/Miunie.Storage/JsonDataStorage.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Miunie.Storage
+{
+    public class JsonDataStorage : IDataStorage
+    {
+    }
+}
