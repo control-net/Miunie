992c1fe773aae99c2d82a8cfa19070df9817b7eb
Carlos J. Gutiérrez Sánchez <cgutisanc@gmail.com>
2019-01-05 21:16:10 +0100

    Implement JsonDataStorage

--- src/Miunie.Storage/JsonDataStorage.cs
+++ src/Miunie.Storage/JsonDataStorage.cs
@@ -1,8 +1,52 @@
 ﻿using System;
+using System.IO;
+using Miunie.Core.Storage;
+using Newtonsoft.Json;
 
 namespace Miunie.Storage
 {
     public class JsonDataStorage : IDataStorage
     {
+        private readonly string resourcesFolder = "Resources";
+
+        public JsonDataStorage()
+        {
+            Directory.CreateDirectory(resourcesFolder);
+        }
+
+        public void StoreObject(object obj, string file)
+        {
+            string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            string filePath = String.Concat(resourcesFolder, "/", file);
+            File.WriteAllText(filePath, json);
+        }        
+
+        public T RestoreObject<T>(string file)
+        {
+            string json = GetOrCreateFileContents(file);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public bool KeyExists(string key)
+        {
+            return LocalFileExists(key);
+        }
+
+        private bool LocalFileExists(string file)
+        {
+            string filePath = String.Concat(resourcesFolder, "/", file);
+            return File.Exists(filePath);
+        }
+
+        private string GetOrCreateFileContents(string file)
+        {
+            string filePath = String.Concat(resourcesFolder, "/", file);
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, "");
+                return "";
+            }
+            return File.ReadAllText(filePath);
+        }
     }
 }
