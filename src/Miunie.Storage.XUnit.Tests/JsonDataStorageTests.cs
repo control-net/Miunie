e1cd06fb81caddb1d7a61541b7433cab830d5a0a
Carlos J. Gutiérrez Sánchez <cgutisanc@gmail.com>
2019-01-23 21:21:35 +0100

    Remove test coupling and write a test for KeyExists

--- src/Miunie.Storage.XUnit.Tests/JsonDataStorageTests.cs
+++ src/Miunie.Storage.XUnit.Tests/JsonDataStorageTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Miunie.Storage.XUnit.Tests
@@ -6,56 +8,85 @@ namespace Miunie.Storage.XUnit.Tests
     public class JsonDataStorageTests : IClassFixture<JsonDataStorageFixture>
     {
         private JsonDataStorageFixture _storageFixture;
-        private readonly object _objectToSave;
-        private readonly string _collection;
+        private readonly object objectToSave;
+        private readonly string ResourcesFolder;
+        private readonly string Collection;
 
         public JsonDataStorageTests(JsonDataStorageFixture storageFixture)
         {
             _storageFixture = storageFixture;
-            _objectToSave = "hello";
-            _collection = "Welcome";
+            ResourcesFolder = _storageFixture.ResourcesFolder;
+            objectToSave = "hello";
+            Collection = "Welcome";
         }
 
         [Fact]
-        public void ShouldStoreAndRestoreObject()
-        {
-            string fileName = DateTimeOffset.Now
-                .ToUnixTimeMilliseconds()
-                .ToString();
-
-            ShouldStoreObject(_objectToSave, _collection, fileName);
-            ShouldRestoreExistingObject(_objectToSave, _collection, fileName);
-            ShouldRestoreCollection(_collection);
-        }
-
-        private void ShouldRestoreCollection(string collection)
+        public void ShouldRestoreCollection()
         {
             var objects = _storageFixture.Storage
-                .RestoreCollection<object>(collection);
+                .RestoreCollection<object>(Collection);
 
-            Assert.NotEmpty(collection);
+            Assert.NotEmpty(objects);
         }
 
-        private void ShouldStoreObject(
-            object obj,
-            string collection,
-            string file)
+        [Fact]
+        public void ShouldStoreObject()
         {
-            _storageFixture.Storage.StoreObject(obj, collection, file);
+            string file = GenerateFileName();
 
-            Assert.True(_storageFixture.Storage.KeyExists(collection, file));
+            _storageFixture.Storage.StoreObject(objectToSave, Collection, file);
+
+            Assert.True(ObjectExists(file));
         }
 
-        private void ShouldRestoreExistingObject(
-            object expected,
-            string collection,
-            string key)
+        [Fact]
+        public void ShouldRestoreExistingObject()
         {
-            var restoredObject = _storageFixture.Storage
-                .RestoreObject<object>(collection, key);
+            int expected = 53415;
 
-            Assert.Equal(expected, restoredObject);
+            var file = SaveObjectInTestCollection(expected);
+            var actual = _storageFixture.Storage
+                .RestoreObject<int>(Collection, file);
+            
+            Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        private void KeyShouldExist()
+        {
+            var obj = "a object";
+            var file = SaveObjectInTestCollection(obj);
+
+            var expected = ObjectExists(file);
+            var actual = _storageFixture.Storage.KeyExists(Collection, file);
+
+            Assert.True(expected == actual);
+        }
+
+        private bool ObjectExists(string key)
+        {
+            var file = String.Concat(key, ".json");
+            var path = Path.Combine(ResourcesFolder, Collection, file);
+            return File.Exists(path);
+        }
+        private string SaveObjectInTestCollection(object obj)
+        {
+            GenerateCollectionDirectory(Collection);
+            string file = GenerateFileName();
+            string json = JsonConvert.SerializeObject(obj);
+            string filePath = Path.Combine(ResourcesFolder, Collection, file);
+            string pathWithExtension = String.Concat(filePath, ".json");
+            File.WriteAllText(pathWithExtension, json);
+            return file;
+        }
+
+        private void GenerateCollectionDirectory(string collection)
+        {
+            var collectionDir = Path.Combine(ResourcesFolder, collection);
+            Directory.CreateDirectory(collectionDir);
+        }
+
+        private string GenerateFileName()
+            => DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString();
     }
 }
-
