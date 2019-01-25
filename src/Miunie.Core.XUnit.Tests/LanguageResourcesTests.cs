4dbc450ab2465ef6d5363832f049739e6383db9b
Carlos J. Gutiérrez Sánchez <cgutisanc@gmail.com>
2019-01-25 22:05:45 +0100

    Implement ShouldGetPhrase test

--- src/Miunie.Core.XUnit.Tests/LanguageResourcesTests.cs
+++ src/Miunie.Core.XUnit.Tests/LanguageResourcesTests.cs
@@ -6,9 +6,33 @@ namespace Miunie.Core.XUnit.Tests
 {
     public class LanguageResourcesTests
     {
-        LanguageResourcesTests()
+        LanguageResources langResources;
+        DataStorageMock storage;
+        string languageCollection = "Lang";
+        string PhraseKey = "HELLO_WORLD";
+        string PhraseValue = "Hello world";
+        string FormattedKey = "WELCOME_MESSAGE";
+        string FormattedValue = "Hello, {0}";
+
+        public LanguageResourcesTests()
         {
+            storage = new DataStorageMock();
+            langResources = new LanguageResources(storage);
+            InitializeStorage();
         }
 
+        private void InitializeStorage()
+        {
+            storage.StoreObject(PhraseValue, languageCollection, PhraseKey);
+            storage.StoreObject(FormattedValue, languageCollection, FormattedKey);
+        }
+        
+        [Fact]
+        public void ShouldGetPhrase()
+        {
+            var actual = langResources.GetPhrase(PhraseKey);
+            var expected = PhraseValue;
+            Assert.Equal(actual, expected);
+        }
     }
 }
