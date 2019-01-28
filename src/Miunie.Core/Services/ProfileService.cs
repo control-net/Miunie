11cdded3cb801c434f8b5a339366e278bec4a157
Carlos J. Gutiérrez Sánchez <cgutisanc@gmail.com>
2019-01-28 02:12:37 +0100

    Modify ProfileService to use LanguageResources

--- src/Miunie.Core/Services/ProfileService.cs
+++ src/Miunie.Core/Services/ProfileService.cs
@@ -6,27 +6,20 @@ namespace Miunie.Core
     public class ProfileService
     {
         private readonly IDiscordMessages _discordMessages;
+        private readonly ILanguageResources _lang;
 
-        public ProfileService(IDiscordMessages discordMessages)
+        public ProfileService(
+                IDiscordMessages discordMessages,
+                ILanguageResources lang)
         {
             _discordMessages = discordMessages;
+            _lang = lang;
         }
 
         public async Task ShowProfile(MiunieUser u, MiunieChannel c)
-        {
-            // TODO(Charly): Replace the following code with the commented code,
-            // once the new service collection is implemented and the discord
-            // wrapper updated to v4:
-
-            // var rep = u.Reputation;
-            // var response = _langResources.GetFormatted("SHOW_PROFILE", rep);
-            // await _discordMessages.SendMessage(response, c);
-
-            var response = ":frame_photo: **USER PROFILE**\n";
-            response += $"Reputation: {u.Reputation}";
-
+        {            
+            var response = _lang.GetFormatted("SHOW_PROFILE", u.Reputation);
             await _discordMessages.SendMessage(response, c);
         }
     }
 }
-
