93a13f288718cccedc5545de9e0de828d2c50776
petrspelos <petr.spelos@gmail.com>
2019-03-24 14:01:05 +0100

    Refactor LanguageResources

--- src/Miunie.Core/Language/ILanguageResources.cs
+++ src/Miunie.Core/Language/ILanguageResources.cs
@@ -3,6 +3,5 @@ namespace Miunie.Core.Language
     public interface ILanguageResources
     {
         string GetPhrase(string key, params object[] objs);
-        void SetLanguage(string langKey);
     }
 }
