0a04a3e1af9752065319d49437b92419511d233d
petrspelos <petr.spelos@gmail.com>
2019-04-13 11:36:09 +0200

    Add Miunie ASCII for console app

--- src/Miunie.ConsoleApp/Program.cs
+++ src/Miunie.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Miunie.Core;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,8 +8,28 @@ namespace Miunie.ConsoleApp
     internal static class Program
     {
         private static async Task Main()
-             => await ActivatorUtilities
-                 .CreateInstance<MiunieBot>(InversionOfControl.Provider)
-                 .RunAsync();
+        {
+            PrintHeader();
+            await ActivatorUtilities
+                .CreateInstance<MiunieBot>(InversionOfControl.Provider)
+                .RunAsync();
+        }
+
+        private static void PrintHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(@"
+    __  __ _             _      
+   |  \/  (_)           (_)     
+   | \  / |_ _   _ _ __  _  ___ 
+   | |\/| | | | | | '_ \| |/ _ \
+   | |  | | | |_| | | | | |  __/
+   |_|  |_|_|\__,_|_| |_|_|\___|
+
+              The community-made Discord bot
+
+");
+            Console.ResetColor();
+        }
     }
 }
