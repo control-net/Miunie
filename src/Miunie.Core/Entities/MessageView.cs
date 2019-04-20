3eab0bf870d82e374cd4ecc4f26ebeceb7f25910
Igor <xigorziomx@gmail.com>
2019-04-21 00:04:54 +0200

    Add message display style
    Add avatar and timestamp to message display.

--- src/Miunie.Core/Entities/MessageView.cs
+++ src/Miunie.Core/Entities/MessageView.cs
@@ -1,9 +1,13 @@
-﻿namespace Miunie.Core
+﻿using System;
+using System.Drawing;
+
+namespace Miunie.Core
 {
     public class MessageView
     {
         public string AuthorName { get; set; }
         public string AuthorAvatarUrl { get; set; }
         public string Content { get; set; }
+        public DateTimeOffset TimeStamp { get; set; }
     }
 }
