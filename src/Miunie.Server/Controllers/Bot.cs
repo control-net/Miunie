0dbb6f196d66a2578d2c17266dd7bcf45468800e
petrspelos <petr.spelos@gmail.com>
2019-04-20 18:19:20 +0200

    Add ASP.NET project base

---
+++ src/Miunie.Server/Controllers/Bot.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Miunie.Server.Controllers
+{
+    public class Bot : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
