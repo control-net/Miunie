f4619f4fdeb05229af105127e9422b68db174692
Alexander Gipp <gipp81@gmail.com>
2019-05-02 21:22:24 +0200

    added creation Date to the Profile Command OWO <3 Peter

--- src/Miunie.Core/Strings.Designer.cs
+++ src/Miunie.Core/Strings.Designer.cs
@@ -19,7 +19,7 @@ namespace Miunie.Core {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Strings {
@@ -147,6 +147,15 @@ namespace Miunie.Core {
         public static string SHOW_REMOTE_REPO {
             get {
                 return ResourceManager.GetString("SHOW_REMOTE_REPO", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to :calendar_spiral: Creation date.
+        /// </summary>
+        public static string USER_EMBED_CREATED_AT_TITLE {
+            get {
+                return ResourceManager.GetString("USER_EMBED_CREATED_AT_TITLE", resourceCulture);
             }
         }
         
