9a09f0a4fe58fb0171123acee18aeb43ab719be1
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-07-07 14:13:01 +0200

    Update TokenValidator.cs (#184)
    Adding an extra check that's always true. Yes, I'm aware this comes out of the blue.

--- src/Miunie.WindowsApp/Utilities/TokenValidator.cs
+++ src/Miunie.WindowsApp/Utilities/TokenValidator.cs
@@ -5,6 +5,6 @@ namespace Miunie.WindowsApp.Utilities
     public class TokenValidator
     {
         public bool StringHasValidTokenStructure(string possibleToken)
-            => possibleToken.Length == 59 && possibleToken.ElementAt(24) == '.';
+            => possibleToken.Length == 59 && possibleToken.ElementAt(24) == '.' && possibleToken.ElementAt(31) == '.';
     }
 }
