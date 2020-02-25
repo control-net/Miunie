c3413423022d394bde1c234635abdcaaa4954fbe
draxcodes <joel@drax.codes>
2020-02-25 22:43:58 +0000

    Implement Sample View

--- src/Miunie.Avalonia/ViewModels/MainWindowViewModel.cs
+++ src/Miunie.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using ReactiveUI;
 
 namespace Miunie.Avalonia.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Hello World!";
+        private string _text = "0";
+
+        public string Text 
+        { 
+            get { return _text; }
+            set { this.RaiseAndSetIfChanged(ref _text, value); }
+        }
+
+        private int num = 1;
+        public void OnClickCommand()
+        {
+            Text = $"{num}";
+            num++;
+        }
     }
 }
--- src/Miunie.Avalonia/Views/MainWindow.xaml.cs
+++ src/Miunie.Avalonia/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Miunie.Avalonia.Views
