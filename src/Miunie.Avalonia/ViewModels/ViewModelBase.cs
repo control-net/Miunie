530008c58888a4c0bc36ae1db2d836a6f7570015
draxcodes <joel@drax.codes>
2020-02-25 21:10:00 +0000

    Add Avalonia Project

---
+++ src/Miunie.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Miunie.Avalonia.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
