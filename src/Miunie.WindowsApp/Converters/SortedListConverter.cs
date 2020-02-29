2838fc947705e360c3d5a51f813a27b84d3ba964
Edd <edd@1n5an1ty.com>
2020-02-29 00:45:59 -0500

    Add sorted list converter Add converter to app.xaml Update ImpersonationChatPage message list to use sortedlist converter

---
+++ src/Miunie.WindowsApp/Converters/SortedListConverter.cs
@@ -0,0 +1,28 @@
+﻿using Miunie.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Miunie.WindowsApp.Converters
+{
+    public class SortedListConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is IEnumerable<MessageView>)
+            {
+                value = (value as IEnumerable<MessageView>).OrderBy(x => x.TimeStamp);
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
