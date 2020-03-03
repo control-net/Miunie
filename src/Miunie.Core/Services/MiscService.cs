bc1c79956235ac4fb9cccaf0289a60b0db7a6de3
petrspelos <petrspelos@users.noreply.github.com>
2020-03-03 16:48:49 +0100

    Add tests for YesNoService

--- src/Miunie.Core/Services/MiscService.cs
+++ src/Miunie.Core/Services/MiscService.cs
@@ -2,8 +2,6 @@
 using Miunie.Core.Discord;
 using Miunie.Core.Entities;
 using Miunie.Core.Entities.Discord;
-using Miunie.Core.Infrastructure;
-using Miunie.Core.Providers;
 using System.Threading.Tasks;
 
 namespace Miunie.Core
@@ -12,17 +10,13 @@ namespace Miunie.Core
     public class MiscService
     {
         private readonly IDiscordMessages _messages;
-        private readonly IDateTime _dateTime;
-        private readonly IMiunieUserProvider _users;
 
-        public MiscService(IDiscordMessages messages, IDateTime dateTime, IMiunieUserProvider users)
+        public MiscService(IDiscordMessages messages)
         {
             _messages = messages;
-            _dateTime = dateTime;
-            _users = users;
         }
 
-        public async Task SendRandomYesNoAnswer(MiunieChannel channel)
+        public async Task SendRandomYesNoAnswerAsync(MiunieChannel channel)
         {
             await _messages.SendMessageAsync(channel, PhraseKey.YES_NO_MAYBE);
         }
