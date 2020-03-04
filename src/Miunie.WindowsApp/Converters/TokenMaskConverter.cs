cf747ce93470e27a53d39e16ed3e30789bd61aa8
Edd <edd@1n5an1ty.com>
2020-03-04 00:44:46 -0500

    Add token mask converter to mask token in UI

---
+++ src/Miunie.WindowsApp/Converters/TokenMaskConverter.cs
@@ -0,0 +1,28 @@
+﻿using Miunie.Core.Entities.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml.Data;
+
+namespace Miunie.WindowsApp.Converters
+{
+    public class TokenMaskConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string token)
+            {
+                if (token == "[NOT CONFIGURED]") return value;
+
+                value = $"{token.Substring(0, 5)} ****************************** {token.Substring(token.Length - 5)}";
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
