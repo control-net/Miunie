c4c953c487c975510150d2384770d5d11c0671e7
Edd <edd@1n5an1ty.com>
2020-03-05 00:20:50 -0500

    Create ObservableMessageView and EnterKeyHelper for dependency property

---
+++ src/Miunie.WindowsApp/Models/ObservableMessageView.cs
@@ -0,0 +1,56 @@
+﻿using GalaSoft.MvvmLight;
+using Miunie.Core.Entities.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miunie.WindowsApp.Models
+{
+    public class ObservableMessageView : ObservableObject
+    {
+        private string _authorName;
+        public string AuthorName
+        {
+            get { return _authorName; }
+            set { 
+                _authorName = value;
+                RaisePropertyChanged(nameof(AuthorName));
+            }
+        }
+
+        private string _authorAvatarUrl;
+        public string AuthorAvatarUrl
+        {
+            get { return _authorAvatarUrl; }
+            set
+            {
+                _authorAvatarUrl = value;
+                RaisePropertyChanged(nameof(AuthorAvatarUrl));
+            }
+        }
+
+        private string _content;
+        public string Content
+        {
+            get { return _content; }
+            set
+            {
+                _content = value;
+                RaisePropertyChanged(nameof(Content));
+            }
+        }
+
+        private DateTimeOffset _timeStamp;
+        public DateTimeOffset TimeStamp
+        {
+            get { return _timeStamp; }
+            set
+            {
+                _timeStamp = value;
+                RaisePropertyChanged(nameof(TimeStamp));
+            }
+        }
+    }
+}
---
+++ src/Miunie.WindowsApp/Utilities/EnterKeyHelpers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace Miunie.WindowsApp.Utilities
+{
+    public static class EnterKeyHelpers
+    {
+        public static ICommand GetEnterKeyCommand(DependencyObject target)
+        {
+            return (ICommand)target.GetValue(EnterKeyCommandProperty);
+        }
+
+        public static void SetEnterKeyCommand(DependencyObject target, ICommand value)
+        {
+            target.SetValue(EnterKeyCommandProperty, value);
+        }
+
+        public static readonly DependencyProperty EnterKeyCommandProperty =
+            DependencyProperty.RegisterAttached(
+                "EnterKeyCommand",
+                typeof(ICommand),
+                typeof(EnterKeyHelpers),
+                new PropertyMetadata(null, OnEnterKeyCommandChanged));
+
+        static void OnEnterKeyCommandChanged(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            ICommand command = (ICommand)e.NewValue;
+            FrameworkElement fe = (FrameworkElement)target;
+            Control control = (Control)target;
+            control.KeyUp += (s, args) =>
+            {
+                if (args.Key == Windows.System.VirtualKey.Enter)
+                {
+                    // make sure the textbox binding updates its source first
+                    BindingExpression b = control.GetBindingExpression(TextBox.TextProperty);
+                    if (b != null)
+                    {
+                        b.UpdateSource();
+                    }
+                    command.Execute((control as TextBox).Text);
+                }
+            };
+        }
+    }
+}
