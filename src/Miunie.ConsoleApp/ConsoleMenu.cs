680d39af36609b8054dd2f7373790199f0e476be
akaMackiee <46831591+akaMackiee@users.noreply.github.com>
2020-03-17 14:52:14 +0100

    Paging for console menu (#292)
    * Implement paging system for ConsoleMenu
    
    * Implement adaptive Pagesize
    
    * Replace two if statements with Math.Clamp method
    
    * Remove Math.Clamp method as it was not needed
    
    * Revert back to Match.Clamp and fix small bug
    
    * Delete ConsoleMenu.cs
    
    * Revert "Delete ConsoleMenu.cs"
    
    This reverts commit 5a3cf0ae40f70bb2ddf257b06ec07e0c0e08059d.
    
    * Revert back to Match.Clamp method and fix small bug
    
    Co-authored-by: Petr Sedláček <petrspelos@users.noreply.github.com>

--- src/Miunie.ConsoleApp/ConsoleMenu.cs
+++ src/Miunie.ConsoleApp/ConsoleMenu.cs
@@ -25,9 +25,15 @@ namespace Miunie.ConsoleApp
 
         private readonly Func<T, string> _formatter;
 
+        private readonly int _outstandingLines = 7;
+
         private string _title;
 
         private int _selectedIndex;
+
+        private int _selectedPageIndex;
+
+        private int _pageSize;
 
         internal ConsoleMenu(IEnumerable<T> items, Func<T, string> formatter)
         {
@@ -47,11 +53,14 @@ namespace Miunie.ConsoleApp
                 return _items.First();
             }
 
+            SetPageSize();
+
             _selectedIndex = 0;
             ConsoleKeyInfo keyPressed;
 
             do
             {
+                SetPageSize();
                 PrintTitle();
                 PrintItems();
 
@@ -62,25 +71,53 @@ namespace Miunie.ConsoleApp
             }
             while (keyPressed.Key != ConsoleKey.Enter);
 
+            _selectedIndex = (_pageSize * _selectedPageIndex) + _selectedIndex;
             return _items.ElementAt(_selectedIndex);
         }
 
         private void HandleMovementKey(ConsoleKeyInfo keyPressed)
         {
+            if (keyPressed.Key == ConsoleKey.UpArrow)
+            {
+                if (--_selectedIndex == -1)
+                {
+                    _selectedIndex = Paginator.GroupAt(_items, _selectedPageIndex, _pageSize).Count() - 1;
+                }
+            }
+
             if (keyPressed.Key == ConsoleKey.DownArrow)
             {
-                if (++_selectedIndex == _items.Count()) { _selectedIndex = 0; }
+                if (++_selectedIndex >= Paginator.GroupAt(_items, _selectedPageIndex, _pageSize).Count())
+                {
+                    _selectedIndex = 0;
+                }
             }
-            else if (keyPressed.Key == ConsoleKey.UpArrow)
+
+            if (keyPressed.Key == ConsoleKey.LeftArrow)
             {
-                if (--_selectedIndex == -1) { _selectedIndex = _items.Count() - 1; }
+                if (_selectedPageIndex != 0)
+                {
+                    _selectedPageIndex--;
+                    _selectedIndex = 0;
+                }
+            }
+
+            if (keyPressed.Key == ConsoleKey.RightArrow)
+            {
+                if (_selectedPageIndex != Paginator.GetPageCount(_items.Count(), _pageSize) - 1)
+                {
+                    _selectedPageIndex++;
+                    _selectedIndex = 0;
+                }
             }
         }
 
         private void PrintItems()
         {
-            var items = _items.Select((item, i) => (i == _selectedIndex ? "=>" : string.Empty) + _formatter.Invoke(item));
+            var itemsInPage = Paginator.GroupAt(_items, _selectedPageIndex, _pageSize);
+            var items = itemsInPage.Select((item, i) => (i == _selectedIndex ? "=>" : string.Empty) + _formatter.Invoke(item));
             Console.WriteLine(string.Join("\n", items));
+            PrintMenuInfo();
         }
 
         private void PrintTitle()
@@ -90,6 +127,33 @@ namespace Miunie.ConsoleApp
                 Console.WriteLine(_title);
                 Console.WriteLine();
             }
+        }
+
+        private void PrintMenuInfo()
+        {
+            PrintToBottom(Paginator.GetPageFooter(_selectedPageIndex, _items.Count(), _pageSize) + "\n\n" + ConsoleStrings.USE_ARROWKEYS);
+        }
+
+        private void PrintToBottom(string message)
+        {
+            var prevCursorLeft = Console.CursorLeft;
+            var prevCursorTop = Console.CursorTop;
+            var lowestLine = Console.WindowHeight - 4;
+            if (lowestLine < 0)
+            {
+                lowestLine = 0;
+            }
+
+            Console.SetCursorPosition(0, lowestLine);
+            Console.Write(message);
+            Console.SetCursorPosition(prevCursorLeft, prevCursorTop);
+        }
+
+        private void SetPageSize()
+        {
+            _pageSize = Console.WindowHeight;
+            _pageSize -= _outstandingLines;
+            _pageSize = Math.Clamp(_pageSize, 2, _items.Count());
         }
     }
 }
